f4f7b523c78ac9db1d13dd177cdbecfa47753c7a
ezramcmh <110797638+ezramcmh@users.noreply.github.com>
2022-11-22 17:58:19 +0700

    Update pencocokan.py

--- lib/pencocokan.py
+++ lib/pencocokan.py
@@ -6,11 +6,11 @@ import numpy as np
 
 K = 37
 IMG_PER_FOLDER = 5
-THRESHOLD = 2000000
+THRESHOLD = 5000
 
 
 def get_eigenfaces(dataset_folder):
-    images = load_image_data(
+    images, image_paths = load_image_data(
         dataset_folder, recursive=True, max_img_per_folder=IMG_PER_FOLDER
     )
     gamma = []
@@ -34,34 +34,35 @@ def get_eigenfaces(dataset_folder):
         eigenfaces.append(A @ v[i])
 
     order = np.argsort(e)[::-1]
-    return np.array(eigenfaces)[order][:K], images, normalized_faces, avg_face
+    return np.array(eigenfaces)[order][:K], images, normalized_faces, avg_face, image_paths
 
 
 def test_image(dataset, filepath):
+    print(filepath)
     test_image = cv2.imread(filepath, 0)
-    test_image = cv2.resize(test_image, (100, 100), interpolation=cv2.INTER_AREA)
+    test_image = cv2.resize(test_image, (100, 100), interpolation=cv2.INTER_CUBIC)
     test_image = np.reshape(test_image, (10000, 1))
-    eigenfaces, images, normalized_faces, avg_face = get_eigenfaces(dataset)
+    eigenfaces, images, normalized_faces, avg_face, image_paths = get_eigenfaces(dataset)
     phi = test_image - avg_face
     test_image_weights = []
-    for i in range(K):
+    for i in range(min(K, len(images))):
         test_image_weights.append(eigenfaces[i].T @ phi)
     test_image_weights = np.row_stack(test_image_weights)
     omega = []
     print(len(images))
     for i in range(len(images)):
         weights = []
-        for j in range(K):
+        for j in range(min(K, len(images))):
             weights.append(eigenfaces[j].T @ normalized_faces[i])
         omega.append(np.row_stack(weights))
-    idxmin = 0
-    euclid = np.linalg.norm(omega[idxmin] - test_image_weights)
+    idxmin = -1
+    euclid = 999999999999
     for i in range(len(omega)):
         euclid_i = np.linalg.norm(omega[i] - test_image_weights)
-        if euclid_i < euclid:
+        if euclid_i < euclid and euclid_i <= THRESHOLD:
             idxmin = i
             euclid = euclid_i
-    return images[idxmin]
+    return image_paths[idxmin] #if idxmin != -1 else ''
 
 
 def euclidean_distance(a, b):
@@ -71,13 +72,11 @@ def euclidean_distance(a, b):
         sum += (a[i][0] - b[i][0]) ** 2
     return np.sqrt(sum)
 
-
+'''
 if __name__ == "__main__":
     img = test_image(
         "dataset",
-        "/home/msfir/Documents/Tubes/Algeo/Algeo02-21042/dataset/105_classes_pins_dataset/pins_Chris Pratt/Chris Pratt60_870.jpg",
+        "D:\Tubes algeo\dataset\pins_Tom Hiddleston\Tom Hiddleston18_1485.jpg",
     )
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.imshow("cocok", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    print(img)
+'''
