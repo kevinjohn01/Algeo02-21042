38e043a5a93bdbabef017ad29f66d6ceaa338493
ezramcmh <110797638+ezramcmh@users.noreply.github.com>
2022-11-26 13:48:08 +0700

    kelupaan T_T

--- src/imgprocess.py
+++ src/imgprocess.py
@@ -6,18 +6,20 @@ import os
 
 def load_image_data(
     folder: str, recursive: bool = False, max_img_per_folder: int = -1
-) -> np.ndarray:
+):
     prev_dir = os.path.abspath(".")
-    data: List[np.ndarray] = []
+    data = []
+    image_paths = []
     if recursive:
         for dirpath, _, files in os.walk(folder):
             print(dirpath)
             count = 0
             for file in files:
-                filepath = os.path.join(dirpath, file)
+                filepath = os.path.abspath(os.path.join(dirpath, file))
                 img = cv2.resize(
-                    cv2.imread(filepath, 0), (100, 100), interpolation=cv2.INTER_AREA
+                    cv2.imread(filepath, 0), (100, 100), interpolation=cv2.INTER_CUBIC
                 )
+                image_paths.append(filepath)
                 data.append(img)
                 count += 1
                 if count == max_img_per_folder:
@@ -26,12 +28,8 @@ def load_image_data(
         os.chdir(folder)
         for file in os.listdir():
             img = cv2.resize(
-                cv2.imread(file, 0), (100, 100), interpolation=cv2.INTER_AREA
+                cv2.imread(file, 0), (100, 100), interpolation=cv2.INTER_CUBIC
             )
             data.append(img)
     os.chdir(prev_dir)
-    return np.array(data, dtype=np.uint8)
-
-
-def matrix_mean(mat: np.ndarray) -> np.ndarray:
-    return np.mean(mat, axis=0, dtype=np.float64)
+    return np.array(data), image_paths
